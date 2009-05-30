c3c53c3dc1a790591eb25817920c6f58095aef08
Daniel James <daniel@calamity.org.uk>
2009-05-30 13:01:55 +0000

    Implement static/lexical scoping for 1.5. Refs #2034.
    [SVN r53451]

--- detail/template_stack.hpp
+++ detail/template_stack.hpp
@@ -21,6 +21,8 @@
 
 namespace quickbook
 {
+    struct template_scope;
+
     //  template symbols are stored as follows:
     //
     //      template name
@@ -29,16 +31,31 @@ namespace quickbook
     //      ...
     //      template param name[N]
     //      template body
+    //      template scope (only used for 1.5+, 1.4- uses the dynamic scope)
 
     typedef boost::tuple<
             std::vector<std::string>
-          , boost::spirit::classic::file_position>
+          , boost::spirit::classic::file_position
+          , template_scope const*>
     template_symbol;
 
     typedef boost::spirit::classic::symbols<template_symbol> template_symbols;
     
+    // template scope
+    //
+    // 1.4-: parent_scope is the previous scope on the stack
+    //       (the template's dynamic parent).
+    // 1.5+: parent_scope is the template's lexical parent.
+    //
+    // This means that a search along the parent_scope chain will follow the
+    // correct lookup chain for that version of quickboook.
+    //
+    // symbols contains the templates defined in this scope.
+    
     struct template_scope
     {
+        template_scope() : parent_scope() {}
+        template_scope const* parent_scope;
         template_symbols symbols;
     };
 
@@ -60,8 +77,7 @@ namespace quickbook
                 // search all scopes for the longest matching symbol.
                 typename Scanner::iterator_t f = scan.first;
                 std::ptrdiff_t len = -1;
-                for (template_stack::deque::const_iterator i = ts.scopes.begin();
-                    i != ts.scopes.end(); ++i)
+                for (template_scope const* i = &*ts.scopes.begin(); i; i = i->parent_scope)
                 {
                     boost::spirit::classic::match<> m = i->symbols.parse(scan);
                     if (m.length() > len)
@@ -80,9 +96,15 @@ namespace quickbook
         template_symbol* find(std::string const& symbol) const;
         template_symbol* find_top_scope(std::string const& symbol) const;
         template_symbols const& top() const;
+        template_scope const& top_scope() const;
+        // Add the given template symbol to the current scope.
+        // If it doesn't have a scope, sets the symbol's scope to the current scope.
         void add(std::string const& symbol, template_symbol const& ts);
         void push();
         void pop();
+
+        // Set the current scope's parent.
+        void set_parent_scope(template_scope const&);
 
         boost::spirit::classic::functor_parser<parser> scope;
 
