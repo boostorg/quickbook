6cacc8bfe4d5ee53d3503ed9be04d3b1ffb7352a
Daniel James <daniel@calamity.org.uk>
2013-12-05 00:32:06 +0000

    Update version.

--- src/quickbook.cpp
+++ src/quickbook.cpp
@@ -40,7 +40,7 @@
 #pragma warning(disable:4355)
 #endif
 
-#define QUICKBOOK_VERSION "Quickbook Version 1.6.1 dev"
+#define QUICKBOOK_VERSION "Quickbook Version 1.6.1 alpha 1"
 
 namespace quickbook
 {
