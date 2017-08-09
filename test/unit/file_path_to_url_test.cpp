433295308e11ca8078a185f041f99d676f24d57b
Daniel James <daniel@calamity.org.uk>
2017-08-09 16:12:07 +0100

    Restore file_path_to_url_test

--- test/unit/file_path_to_url_test.cpp
+++ test/unit/file_path_to_url_test.cpp
@@ -7,7 +7,8 @@
 =============================================================================*/
 
 #include <boost/detail/lightweight_test.hpp>
-#include "include_paths.hpp"
+#include <boost/predef.h>
+#include "path.hpp"
 
 int main() {
     using boost::filesystem::path;
@@ -16,8 +17,14 @@ int main() {
     BOOST_TEST_EQ("a/b", file_path_to_url(path("a/b")));
     BOOST_TEST_EQ("../a/b", file_path_to_url(path("../a/b")));
     BOOST_TEST_EQ("A%20B%2bC%2520", file_path_to_url(path("A B+C%20")));
+    BOOST_TEST_EQ("file:///a/b", file_path_to_url(path("/a/b")));
 
     // TODO: Windows specific tests.
+#if BOOST_OS_WINDOWS
+    BOOST_TEST_EQ("file:///a", file_path_to_url(path("\\a")));
+    BOOST_TEST_EQ("file:///c:/", file_path_to_url(path("c:\\")));
+    BOOST_TEST_EQ("file:///c:/foo/bar", file_path_to_url(path("c:\\foo\\bar")));
+#endif
 
     return boost::report_errors();
 }
